c916311f460f88565e972e1d1eadb6c01acfe552
nightspring <codyrayhunt@gmail.com>
2017-03-07 15:15:22 -0800

    Added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "getcookin",
+      script    : "app.js",
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "root",
+      host : "138.68.229.113",
+      ref  : "origin/master",
+      repo : "https://github.com/nightspring/getcookin.git",
+      path : "~/getcookin",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+};
